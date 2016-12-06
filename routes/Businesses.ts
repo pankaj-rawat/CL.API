583447eb73c5d686b9da32c84716c6b0a34f8dc3
your name <your.name@domain.com>
2016-12-06 08:54:03 -0800

    header case change

--- routes/Businesses.ts
+++ routes/Businesses.ts
@@ -103,7 +103,7 @@ businessController.get('/', function (req: express.Request, res: express.Respons
         clRes = { data: result.data, isValid: true };
         var pageLink = util.getPageLinks(util.getURLstring(req), offset, limit, result.recordCount);
         res.links(pageLink);
-        res.setHeader('Content-Range', util.getHeaderContentRange(offset, limit, result.recordCount));
+        res.setHeader('content-range', util.getHeaderContentRange(offset, limit, result.recordCount));
         res.send(clRes);
     })
         repoResponse.catch(function (err) {
